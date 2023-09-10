8e9379336b4fc9b6b01c606dd65e87bbbbcf9794
AVKydin <tlalok@ukr.net>
2023-09-10 14:48:12 +0300

    start

---
+++ src/Main.jsx
@@ -0,0 +1,22 @@
+import {useState} from "react";
+import List from "./components/List";
+
+function Main(){
+
+    const [tasks, setTasks] = useState([{id:0, title:'Clean', status:false}]);
+
+    return (
+        <div className='container'>
+            <h1>Note your tasks</h1>
+
+            <div className='input-field'>
+                 <input type='text'/>
+                <label>Task name</label>
+            </div>
+
+            <List tasks={tasks}/>
+        </div>
+    )
+}
+
+export default Main
