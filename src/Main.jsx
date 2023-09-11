a1be9bd73bf4200f4820ba0b9f88096e621c80fb
AVKydin <tlalok@ukr.net>
2023-09-11 10:15:31 +0300

    end

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,16 +1,46 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import List from "./components/List";
+import {v4 as uuidv4} from 'uuid';
 
 function Main(){
 
-    const [tasks, setTasks] = useState([{id:0, title:'Clean', status:false}]);
+    const [tasks, setTasks] = useState(()=>{
+        const storedTodos = localStorage.getItem('tasks');
+        if(!storedTodos){
+            return [];
+        } else {
+            return JSON.parse(storedTodos)
+        }
+    });
+    const [taskTitle, setTaskTitle] = useState('');
+
+    useEffect(()=>{
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }, [tasks])
+
+    const addTask = (e)=>{
+        const storedTodos = JSON.parse(localStorage.getItem('tasks'));
+        if(e.key === "Enter" && e.target.value !== ''){
+            setTasks([...storedTodos,
+                {id: uuidv4(),
+                 title: taskTitle,
+                 status: false}])
+            setTaskTitle('')
+        }
+    }
+
+    const date = new Date().toLocaleDateString();
 
     return (
         <div className='container'>
             <h1>Note your tasks</h1>
+            <span>{date}</span>
 
             <div className='input-field'>
-                 <input type='text'/>
+                 <input type='text'
+                 value={taskTitle}
+                 onChange={event=>setTaskTitle(event.target.value)}
+                 onKeyDown={addTask}/>
                 <label>Task name</label>
             </div>
 
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -11,17 +11,43 @@ export default function Item({title, id, status}){
 
     const updateStatus = () =>{
         setChecked(!checked)
+        const storedTodos = JSON.parse(localStorage.getItem('tasks'));
+        storedTodos.map((el)=>{
+            if(el.id === id){
+                el.status = !checked;
+            }
+            return true
+        })
+        localStorage.setItem('tasks', JSON.stringify(storedTodos))
+    }
+
+    const [visible, setVisible] = useState(true)
+
+    const removeElement = () =>{
+        setVisible(prevState => !prevState);
+        const storedTodos = JSON.parse(localStorage.getItem('tasks'));
+        let removeTodos = storedTodos.filter(item=>{
+            if(item.id !== id){
+                return true
+            }
+            return false
+        })
+        localStorage.setItem('tasks', JSON.stringify(removeTodos))
     }
 
     return (
-        <li className={classes.join(' ')}>
-            <label>
-                <input type="checkbox"
-                checked={checked}
-                onChange={updateStatus}/>
-                <span>{title}</span>
-                <i className='material-icons red-text'>X</i>
-            </label>
-        </li>
+        <>
+            {visible && (<li className={classes.join(' ')}>
+                <label>
+                    <input type="checkbox"
+                           checked={checked}
+                           onChange={updateStatus}/>
+                    <span>{title}</span>
+                    <i className='material-icons red-text'
+                       onClick={removeElement}>X</i>
+                </label>
+            </li>)}
+
+        </>
     )
 }
